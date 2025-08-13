008f5598e2cc663b3495b71666dea9c5a98a9dee
Pavel Bardov <pbardov@gmail.com>
2025-08-14 01:58:37 +0500

    Реализованы основные методы работы с чеками

--- src/www/www.controller.ts
+++ src/www/www.controller.ts
@@ -1,6 +1,7 @@
-import {Controller, Get, Inject} from '@nestjs/common';
+import {Body, Controller, Get, Inject, Post, Headers, Param, Put} from '@nestjs/common';
 import httpConfig, {HttpConfig} from './http.config.js';
 import AtolKkmService from '../atol-kkm/atol-kkm.service.js';
+import {unflatten} from 'flat';
 
 @Controller()
 export default class WwwController {
@@ -19,5 +20,27 @@ export default class WwwController {
 			return {deviceInfo, deviceStatus};
 		});
 		return {deviceInfo, deviceStatus};
+	}
+
+	@Post('/receipt')
+	async openReceipt(@Body() data: unknown, @Headers('content-format') format: string = 'json') {
+		const procData = format === 'flatten' ? unflatten(data) : data;
+		return this.kkmSvc.openReceipt(procData!);
+	}
+
+	@Get('/receipt/:id')
+	async getReceipt(@Param('id') id: string) {
+		return this.kkmSvc.getReceipt(id);
+	}
+
+	@Put('/receipt/:id')
+	async fiscalizeReceipt(@Param('id') id: string) {
+		return this.kkmSvc.fiscalizeReceipt(id);
+	}
+
+	@Post('/receipt/:id/item')
+	async addReceiptItem(@Body() data: unknown, @Param('id') id: string, @Headers('content-format') format: string = 'json') {
+		const procData = format === 'flatten' ? unflatten(data) : data;
+		return this.kkmSvc.addReceiptItem(id, procData!);
 	}
 }
