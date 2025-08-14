d05f42f94b1aede4336492f3f41b44a196696db6
Pavel Bardov <pbardov@gmail.com>
2025-08-14 12:27:34 +0500

    Реализован FlattenFormatInterceptor

---
+++ src/common/interceptors/flatten-format.interceptor.ts
@@ -0,0 +1,30 @@
+import {CallHandler, ExecutionContext, NestInterceptor} from '@nestjs/common';
+import {map, Observable} from 'rxjs';
+import {flatten, unflatten} from 'flat';
+
+export default class FlattenFormatInterceptor implements NestInterceptor {
+	intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+		const request = context.switchToHttp().getRequest();
+		const contentFormat = request.headers['content-format'];
+		const acceptFormat = request.headers['accept-format'];
+
+		// Обработка входящего запроса (unflatten)
+		if (contentFormat && contentFormat === 'flatten' && request.body) {
+			request.body = unflatten(request.body);
+		}
+
+		// Обработка исходящего ответа (flatten)
+		if (acceptFormat && acceptFormat === 'flatten') {
+			return next.handle().pipe(
+				map(data => {
+					if (data) {
+						return flatten(data);
+					}
+					return data;
+				})
+			);
+		}
+
+		return next.handle();
+	}
+}
